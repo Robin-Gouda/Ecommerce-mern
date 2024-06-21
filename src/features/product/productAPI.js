8ee40e1a7da76b7c42150676f674037e6034816a
Robin Gouda <robin2contact@gmail.com>
2024-06-21 22:21:31 +0530

    product Pages and filter

--- src/features/product/productAPI.js
+++ src/features/product/productAPI.js
@@ -8,14 +8,25 @@ export function fetchAllProducts() {
   });
 }
 
-export function fetchProductsByFilters(filter) {
-  // filter = {"category":"lipstick"}
+export function fetchProductsByFilters(filter, sort) {
+  // filter = {"category":["lipstick","fish",...]}
+  //sort = {_sort:"price" }
+
+  //todo: on server we will support multi values in filter
   let queryString = "";
   for (let key in filter) {
-    queryString += `${key}=${filter[key]}&`;
+    const categoryValues = filter[key];
+    if (categoryValues.length > 0) {
+      const lastCategoryValue = categoryValues[categoryValues.length - 1];
+      queryString += `${key}=${lastCategoryValue}&`;
+    }
+  }
+  for (let key in sort) {
+    queryString += `${key}=${sort[key]}&`;
   }
 
   return new Promise(async (resolve) => {
+    //todo we will  not hard-code server value
     const response = await fetch(
       "http://localhost:3002/products?" + queryString
     );
